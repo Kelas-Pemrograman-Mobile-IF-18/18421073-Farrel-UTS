1d0f10d512fa81aad71659dc1b46b1d0e03eb1af
radenarya11 <radenaryade10@gmail.com>
2021-04-29 07:59:42 +0700

    first commit

---
+++ app/src/main/java/com/farrel/aplikasisports/admin/homeadmin.java
@@ -0,0 +1,16 @@
+package com.farrel.aplikasisports.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.farrel.aplikasisports.R;
+
+public class homeadmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homeadmin);
+    }
+}
